9114fc8f5043fb6cb35b3402780568146eb4e29a
Andrey <andrey9kovlev@mail.ru>
2022-12-06 21:29:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// console.log(1122);
+// let number = 5;
+// number = 7;
+// const leHhah = 6;
+// leHhah = 3;
+// console.log (leHhah);
+
+// let number = 4.6;
+// console.log(4 / 0);
+// console.log("dds" * 3);
+
+// const persone = 'Andr';
+
+// const obj = {
+// name: 'anDR',
+// age: 44,
+// isMarried: false
+// };
+
+// obj.5 = 55;
+
+// // console.log(obj.age);
+// console.log(obj.5);
+
+// let arr = ['sdv', '43', 4, 're'];
+// console.log (arr[0]);
+
+// let fruits = ["Яблоко", "Апельсин", "Слива"];
+// fruits[2] = 'Груша';
+// fruits[3] = 'Груша';
+// console.log( fruits[0] );
+// console.log( fruits[1] );
+// console.log( fruits[2] );
+// console.log( fruits);
+
+// const arrObj = {
+//     g: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
+// arrObj.b = 'jgfhj';
+
+// console.log( arrObj.b);
+
+
+// let ntmber = 5;
+// ntmber = 7;
+// const leHhah = 6;
+// // leHhah = 3;
+// console.log (ntmber);
+
+
+// let user = {};
+
+// user["likes birds"] = true;
+
+// console.log(user[likes birds]); 
+
+// delete user["likes birds"];
+
+// alert ("Hello");
+// let result = confirm("ты педик?");
+// console.log(result);
+//  let answer = +prompt ("вам есть 18?", 18);
+//  console.log(answer+5);
+
+let answers = [];
+// answers[0] = prompt("тВОЕ ИМЯ?", "");
+// answers[1] = prompt("фАМИЛИЯ?", "");
+// answers[2] = prompt("ВОЗРАСТ?", "");
+console.log(typeof (answers));
+console.log(typeof (null));
+
+const same = "folder";
+console.log(`http://${same}/66`);
+
+const user = 'pedik';
+alert (`Привет ${user}`);
